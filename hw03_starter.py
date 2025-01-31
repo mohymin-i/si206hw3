878567b6c3c598e301cd8f7f9007c3a8bfd8abab
mohymin-i <mohymini@umich.edu>
2025-01-31 11:49:36 -0500

    fourth commit

--- hw03_starter.py
+++ hw03_starter.py
@@ -33,9 +33,23 @@ class MagicEightBall:
         self.previous_answers = []
     
     def __str__(self):
-        return ""
+        if (self.previous_questions.len() == 0):
+            return ""
+        else:
+            return ""
     
-    def get_fortune(self):
+
+
+    def get_fortune(self, question):
+        if question in self.previous_questions:
+            return "I've already answered this question"
+        else:
+            self.previous_questions.append(question) # add the question to the list
+            random_ans = random.randint(0, self.answers.len()) # get a random index
+            self.previous_answers.append(random_ans) # add the answer index to previous answers
+            return self.answers[random_ans] # return random index
+
+
         return
     
     def play_game(self):
@@ -45,16 +59,14 @@ class MagicEightBall:
 # Main function
 def main():
     # 1. Define the possible answers into a list: 
-
     # 2. Create a MagicEightBall object
-
     # 3. Print the MagicEightBall object 
-
     # 4. Initiate the game play using the play_game() method
-
     # 5. Call print_answer_frequencies()
-
     # 6. Print the MagicEightBall object 
+
+    
+
     return
 
 
